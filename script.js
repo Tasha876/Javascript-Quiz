022ed77e3f1a5c446141345c948806e5ed7415b7
Natasha Fray <natasha.fray9@gmail.com>
2020-10-30 23:40:23 -0400

    ade script functional, changed styling

--- script.js
+++ script.js
@@ -1,43 +1,35 @@
 var questions = document.querySelectorAll(".question");
-
-console.log(questions)
-
 var questionCount = questions.length;
-
 var atQuestion = 0;
-
 var scramble = scrambleNums(questionCount);
 
-console.log("scram " + scramble);
-
 var finalScore = document.querySelector("#final-score");
-
-for (var i = 0; i < questionCount; i++) {
-  questions[i].classList.add("hide");
-} 
-
-
-
-finalScore.classList.add("hide");
-console.log(finalScore.innerHTML);
-
-
-
+var hasHighscore = document.querySelector("#hasHighscore")
+var submittedHS = false;
 
 var quiz = document.querySelector("#quiz");
+var possibleAnswers = document.querySelectorAll(".answers li");
 
 var time = 120;
 var timer = document.querySelector("#timer");
-
-timer.innerHTML = "2:00";
-
-nextQuestion();
-
 var runClock = setInterval(clock, 1000);
+
+var form = document.querySelector("form");
+var submitBtn = document.querySelector("#submit"); 
+var highscoreList = document.querySelector("#final-score ol");
+var initials = document.querySelector("#initials");
+var highscores = [];
+
+function hideQuestions(){
+  for (var i = 0; i < questionCount; i++) {
+    index = scramble[i]
+    questions[index].classList.add("hide");
+  } 
+}
 
 function clock() {
 
-  time--;
+  if (time) time--;
 
   var minutes = Math.floor(time / 60);
   var seconds = time - minutes * 60;
@@ -45,8 +37,8 @@ function clock() {
   timer.innerHTML = minutes + ":" +  ("00" + seconds).slice(-2);
 
 	if (time <= 0) {
-		endGame()
-		clearInterval(runClock);
+    time = 0;
+		endGame();
 	} else if (time <= 15) {
 		timer.classList.remove("near_end");
 		timer.classList.add("almost_up");
@@ -56,21 +48,19 @@ function clock() {
 }
 
 function updateScore(e) {
+  e.target.classList.add("active");
 	if (!e.target.classList.contains("correct")) {
-		(time - 10 >= 0)? (time -= 9, clock()) : (time = 0, clock());
+    (time - 15 >= 0)? (time -= 15, clock()) : (time = 0, clock());
 	}
 }
 
 function nextQuestion() {
-  //console.log("count" + time)
   var curr = questions[scramble[atQuestion]];
   atQuestion++;
   if (atQuestion < questionCount && time) {
     var next = questions[scramble[atQuestion]];
     curr.classList.add("hide");
-    console.log(questions[scramble[atQuestion - 1]])
     next.classList.remove("hide");
-    console.log("Q", scramble[atQuestion]);
   } else {
     curr.classList.add("hide");
     endGame();
@@ -78,28 +68,27 @@ function nextQuestion() {
 }
 
 function endGame() {
+  hideQuestions();
+  timer.classList.add("stop_blinking");
   var scoreSpan = document.querySelector("#score");
   scoreSpan.innerText = time;
   finalScore.classList.remove("hide");
-  console.log("final " + finalScore.innerHTML);
   clearInterval(runClock);
-  //timer.classList.add("hide");
+  init();
 }
 
-var possibleAnswers = document.querySelectorAll(".answers li");
-
 for (let i = 0; i < possibleAnswers.length; i++) {
-  possibleAnswers[i].addEventListener("click",updateScore);
+  possibleAnswers[i].addEventListener("mousedown",updateScore);
   possibleAnswers[i].addEventListener("click",nextQuestion);
 }
 
-function scrambleNums(num){
+function scrambleNums(num) {
   var scrambledList = [];
   var unscrambledList = []
   for (let i = 0; i < num; i++) {
     unscrambledList.push(i);
   }
-  while(unscrambledList.length){
+  while (unscrambledList.length){
     var len = unscrambledList.length;
     item = unscrambledList[Math.floor(Math.random() * len)];
     scrambledList.push(item);
@@ -108,41 +97,70 @@ function scrambleNums(num){
   return scrambledList;
 }
 
-// var textArea = document.querySelector("form textarea");
-// var initials = document.querySelector("#initials");
-// console.log(initials.value)
-// var submit = document.querySelector("#submit");
-// console.log(submit.innerText)
+function init() {
+  var storedHighscores = JSON.parse(localStorage.getItem("highscores"));
 
+  if (storedHighscores) {
+    highscores = storedHighscores;
+  }
+  renderHighscores();
 
-// function addInitials(e){
-//   e.preventDefault();
-//   if (initials) {
-//     textArea.value += time + ":" + initials.value + "\n";
-//     submit.disabled = true;
-//   }
-// }
+  if (time <= 0 || time < highscores[9][0]) {
+    hasHighscore.innerText = "Better luck next time."
+  }
+}
 
-// submit.addEventListener("submit", function(e) { addInitials(e)} );
+function storeHighscores() {
+  localStorage.setItem("highscores", JSON.stringify(highscores));
+}
 
-var textArea = document.querySelector("form textarea");
-var initials = document.querySelector("#initials");
-console.log(initials.value);
-var highscorers = [];
+function renderHighscores() {
 
-function addInitials(e){
-  e.preventDefault();
-  if (initials) {
-    highscorers.push(initials);
-    sessionStorage.setItem(highscores, JSON.stringify(highscorers));
-    textArea.value += time + ":" + initials.value + "\n";
+  highscoreList.innerText = "";
+  highscores.sort();
+  highscores.reverse();
+
+  for (var i = 0; i < 10; i++) {
+    var li = document.createElement("li");
+    (highscores[i])? li.innerHTML = "<span class = tab>" + highscores[i][1] + "</span>" + "<span class = 'tab alignRight'>" + highscores[i][0] + "</span>" : li.innerHTML = "";
+    highscoreList.appendChild(li);
+  }
+}
+
+function addInitials() {
+  if (initials.value) {
+    highscores.push([time, initials.value]);
+    initials.value = "";
   } 
-  var submitBtn = document.querySelector("#submit") 
+
+  storeHighscores();
+  renderHighscores();
+
   submitBtn.disabled = true;
-  console.log(submit.disabled)
+
   return true;
 }
-var form = document.querySelector("form")
 
-form.addEventListener("submit", function(e) { addInitials(e)});
-console.log(scrambleNums(9));
+hideQuestions();
+
+questions[scramble[0]].classList.remove("hide");
+
+finalScore.classList.add("hide");
+
+timer.innerHTML = "2:00";
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault(); 
+  addInitials()
+  submittedHS = true;
+});
+
+initials.addEventListener("keyup", function(){
+  if (initials.value.length && !submittedHS) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+    
+  } return true;
+  
+});
